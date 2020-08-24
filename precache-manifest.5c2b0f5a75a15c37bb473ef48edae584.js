5746aeb55a36dceb99400de199dd4d1cade34160
RobertZSun <robertsunzhe@hotmail.com>
2020-08-24 18:48:01 -0400

    Updates

---
+++ precache-manifest.5c2b0f5a75a15c37bb473ef48edae584.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1f0264b332141d1cd1afdfaa32702b0",
+    "url": "/monstersSearch/index.html"
+  },
+  {
+    "revision": "77e0129d4f31ab45e508",
+    "url": "/monstersSearch/static/css/main.50ca5e13.chunk.css"
+  },
+  {
+    "revision": "5db257ba903a8fee2e3c",
+    "url": "/monstersSearch/static/js/2.c75ba697.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstersSearch/static/js/2.c75ba697.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77e0129d4f31ab45e508",
+    "url": "/monstersSearch/static/js/main.de5428ad.chunk.js"
+  },
+  {
+    "revision": "63187ecac85ac3d0abb7",
+    "url": "/monstersSearch/static/js/runtime-main.1ff6ce0a.js"
+  }
+]);
